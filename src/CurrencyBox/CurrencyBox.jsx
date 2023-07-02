1d81ddd0174245a858a54be48f7d4ae2d7976ba9
tsopsericado <ricardotsopse@gmail.com>
2023-07-02 01:30:12 +0100

    folder structure and functionality added in the currencybox file

--- src/CurrencyBox/CurrencyBox.jsx
+++ src/CurrencyBox/CurrencyBox.jsx
@@ -51,14 +51,16 @@ function CurrencyBox() {
       const unit1 = `${operation1} USD`;
       setTotal(unit1);
     } else if (values.defaultCurrency === "EUR") {
-      const operation2 =( 
-        message1 * 0.93 + message2 + message3 * 0.01 + message4 * 0.14
-        ).toFixed(2);
-        const unit2 = `${operation2} EUR`;
-        setTotal(unit2);
-    } else if (values.defaultCurrency === 'XAF') {
-      const operation3 =
-      (
+      const operation2 = (
+        message1 * 0.93 +
+        message2 +
+        message3 * 0.01 +
+        message4 * 0.14
+      ).toFixed(2);
+      const unit2 = `${operation2} EUR`;
+      setTotal(unit2);
+    } else if (values.defaultCurrency === "XAF") {
+      const operation3 = (
         message1 * 611.64 +
         message2 * 658.13 +
         message3 +
@@ -76,8 +78,7 @@ function CurrencyBox() {
       const unit4 = `${operation4} CNY`;
       setTotal(unit4);
     }
-    };
-
+  };
 
   function doExchange(event) {
     event.preventDefault();
@@ -86,100 +87,179 @@ function CurrencyBox() {
     const values2 = Object.fromEntries(change.entries());
 
     if (values2.from === values2.too) {
-      alert('currency for exchange are similar');
+      alert("currency for exchange are similar");
     }
 
-    if (values2.too === 'USD') {
-      if (values2.from === 'XAF' && values2.deposite < message3) {
+    if (values2.too === "USD") {
+      if (values2.from === "XAF" && values2.deposite < message3) {
         const val1 = message3 - values2.deposite;
         setMessage3(val1);
         setMessage1(message1 + val1 * 0.01);
-      } else if (values2.from === 'EUR' && values2.deposite < message2) {
+      } else if (values2.from === "EUR" && values2.deposite < message2) {
         const val1 = message2 - values2.deposite;
         setMessage2(val1);
         setMessage1(message1 + val1 * 0.93);
-      } else if (values2.from === 'CNY' && values2.deposite < message4) {
+      } else if (values2.from === "CNY" && values2.deposite < message4) {
         const val1 = message4 - values2.deposite;
         setMessage4(val1);
         setMessage1(message1 + val1 * 0.14);
       } else {
-        alert('Insufficient amount in wallet');
+        alert("Insufficient amount in wallet");
       }
-    } else if (values2.too === 'EUR') {
-      if (values2.from === 'XAF' && values2.deposite < message3) {
+    } else if (values2.too === "EUR") {
+      if (values2.from === "XAF" && values2.deposite < message3) {
         const val1 = message3 - values2.deposite;
         setMessage3(val1);
         setMessage2(message2 + val1 * 0.01);
-      } else if (values2.from === 'USD' && values2.deposite < message1) {
+      } else if (values2.from === "USD" && values2.deposite < message1) {
         const val1 = message1 - values2.deposite;
         setMessage1(val1);
         setMessage2(message2 + val1 * 0.93);
-      } else if (values2.from === 'CNY' && values2.deposite < message4) {
+      } else if (values2.from === "CNY" && values2.deposite < message4) {
         const val1 = message4 - values2.deposite;
         setMessage4(val1);
         setMessage2(message2 + val1 * 0.14);
       } else {
-        alert('Insufficient amount in wallet');
+        alert("Insufficient amount in wallet");
       }
-    } else if (values2.too === 'CNY') {
-      if (values2.from === 'XAF' && values2.deposite < message3) {
+    } else if (values2.too === "CNY") {
+      if (values2.from === "XAF" && values2.deposite < message3) {
         const val1 = message3 - values2.deposite;
         setMessage3(val1);
         setMessage4(message4 + val1 * 0.01);
-      } else if (values2.from === 'USD' && values2.deposite < message1) {
+      } else if (values2.from === "USD" && values2.deposite < message1) {
         const val1 = message1 - values2.deposite;
         setMessage1(val1);
         setMessage4(message4 + val1 * 6.78);
-      } else if (values2.from === 'EUR' && values2.deposite < message2) {
+      } else if (values2.from === "EUR" && values2.deposite < message2) {
         const val1 = message2 - values2.deposite;
         setMessage2(val1);
         setMessage4(message4 + val1 * 7.29);
       } else {
-        alert('Insufficient amount in wallet');
+        alert("Insufficient amount in wallet");
       }
-    } else if (values2.too === 'XAF') {
-      if (values2.from === 'CNY' && values2.deposite < message4) {
+    } else if (values2.too === "XAF") {
+      if (values2.from === "CNY" && values2.deposite < message4) {
         const val1 = message4 - values2.deposite;
         setMessage4(val1);
         setMessage3(message3 + val1 * 90.23);
-      } else if (values2.from === 'USD' && values2.deposite < message1) {
+      } else if (values2.from === "USD" && values2.deposite < message1) {
         const val1 = message1 - values2.deposite;
         setMessage1(val1);
         setMessage3(message3 + val1 * 611.64);
-      } else if (values2.from === 'EUR' && values2.deposite < message2) {
+      } else if (values2.from === "EUR" && values2.deposite < message2) {
         const val1 = message2 - values2.deposite;
         setMessage2(val1);
         setMessage3(message3 + val1 * 658.13);
       } else {
-        alert('Insufficient amount in wallet');
+        alert("Insufficient amount in wallet");
       }
     }
   }
 
- 
-
-
-
-
-
-
-
-
-  
-  
-    return (
-      <div className="main">
-        <div className="main__holder__left">
-          <div className="main__wallet">
-            <i className="fa-solid fa-wallet"/>
-            <h3>Wallet</h3>
+  return (
+    <div className="main">
+      <div className="main__holder__left">
+        <div className="main__wallet">
+          <i className="fa-solid fa-wallet" />
+          <h3>Wallet</h3>
+        </div>
+        <div className="currency__option">
+          <div className="input" type="number" name="USD">
+            {message1}
           </div>
-          <div className="currency__option">
 
+          <span>USD</span>
+        </div>
+        <div className="currency__options">
+          <div className="input" type="number" name="EUR">
+           {message2}
           </div>
+          <span>EUR</span>
+        </div>
+
+        <div className="currency__options">
+          <div className="input" type="number" name="XAF">{message3}
+          </div>
+          <span>XAF</span>
+        </div>
+
+        <div className="currency__options">
+          <div className="input" type="number" name="CNY">
+            {message4}
+          </div>
+          <span>CNY</span>
+        </div>
+
+        
+        <div>
+          <h3>Total Currency</h3>
+          <div className="total__currency">{total}</div>
         </div>
       </div>
-    );
-    
- }
-export default CurrencyBox
+
+        <div className="main__holder__right">
+          <img src={logo2} alt="logo" className="logo2" />
+          <h1>Currency Converter</h1>
+          <h3 className="paragraph">
+            <i>List of tasks yo can perfom</i>
+          </h3>
+          <i><b>NB:</b>Confirm after any Change made</i>
+          <form className="main__holder__deposit" onSubmit={handleSubmit}>
+            <div className="main__heading">
+              <p>
+                <b>Default Currency:</b>
+              </p>
+              <select name="defaultCurrency" id="currencies">
+                <option value="USD">USD</option>
+                <option value="EUR">EUR</option>
+                <option value="XAF">XAF</option>
+                <option value="CNY">CNY</option>
+              </select>
+            </div>
+
+            <h3 className="right__headings">Deposite</h3>
+            <div>
+              <span>Amount:</span>
+              <input type="number" name="Amount"/>
+            </div>
+            <div>
+              <span>Currency for Deposite:</span>
+            <select name="currency" id="currencices2">
+              <option value="USD">USD</option>
+              <option value="EUR">EUR</option>
+              <option value="XAF">XAF</option>
+              <option value="CNY">CNY</option>
+            </select>
+            </div>
+            <button type="submit" className="confirm"> Connfirm</button>
+          </form>
+
+          <form className="main__holder__deposit" onSubmit={doExchange}>
+            <h3 className="right__headings">Exchange Currencies</h3>
+            <div>
+              <span>Amount:</span>
+              <input type="number" name="deposite" />
+            </div>
+            <div className="exchanger">
+              <select name="from" id="currencies2">
+                <option value="USD">USD</option>
+                <option value="EUR">EUR</option>
+                <option value="XAF">XAF</option>
+                <option value="CNY">CNY</option>
+              </select>
+              <i className="fa-sharpfa-solid fa-arrow-right"/>
+              <select name="to" id="currencies2">
+                <option value="XAF">XAF</option>
+                <option value="CNY">CNY</option>
+                <option value="EUR">EUR</option>
+                <option value="USD">USD</option>
+              </select>
+            </div>
+            <button type="submit" className="confirm">Confirm</button>
+          </form>
+        </div>
+      </div>
+  );
+}
+export default CurrencyBox;
