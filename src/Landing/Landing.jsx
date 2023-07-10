56192d1fcab176f59b132da935590dc7fccd629c
tsopsericado <ricardotsopse@gmail.com>
2023-07-10 02:48:13 +0100

    some ui changes made on my landing page

--- src/Landing/Landing.jsx
+++ src/Landing/Landing.jsx
@@ -1,24 +1,32 @@
 import React from 'react'
 import "./Landing.css";
-import { Link } from 'react-router-dom';
-import logo1 from '../images/logo1.png';
+import Navbar from '../Navbar/Navbar';
+import { BsCart2 } from "react-icons/bs";
+import
+
+// import { Link } from 'react-router-dom';
+
+// import logo1 from '../images/logo1.png';
 
 
 export default function Landing() {
   return (
     <div className="container">
-      <div className="home__main">
-        <h1>
-          <i>Welcome to your currency Exchange App </i>
-        </h1>
-        <img src={logo1} alt="money" className="logo" />
-        <h2>click on the button below to get started</h2>
-        <button type="button">
-          <Link to="/currency" className="home__link">
-            Get Started
-          </Link>
-        </button>
-      </div>
+      <Navbar />
     </div>
   );
 }
+
+
+      // <div className="home__main">
+      //   <h1>
+      //     <i>Welcome to your currency Exchange App </i>
+      //   </h1>
+      //   <img src={logo1} alt="money" className="logo" />
+      //   <h2>click on the button below to get started</h2>
+      //   <button type="button">
+      //     <Link to="/currency" className="home__link">
+      //       Get Started
+      //     </Link>
+      //   </button>
+      // </div>;
